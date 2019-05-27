3bf8f3c8dc66bdf8f5845634df56e1d9f3f7d296
ccs <chaichuanshi@sunlands.com>
2019-05-27 14:31:39 +0800

    opt

--- coinapi/src/main/resources/static/js/index.js
+++ coinapi/src/main/resources/static/js/index.js
@@ -5,16 +5,20 @@ $(document).ready(function(){
     $("#crypto").bind("click",function () {
         $("#exchangesTable").hide();
         $("#cryptoTable").show();
-        $("#cryptoTable").addClass("checked");
-        $("#exchangesTable").removeClass("checked");
+        $("#crypto").addClass("checked");
+        $("#exchange").removeClass("checked");
         initCrytoData();
     });
     $("#exchange").bind("click",function () {
-        $("#exchangesTable").addClass("checked");
-        $("#cryptoTable").removeClass("checked");
+        $("#exchange").addClass("checked");
+        $("#crypto").removeClass("checked");
         $("#cryptoTable").hide();
         $("#exchangesTable").show();
         initExchangesData();
+    });
+    $("#lang").change(function(){
+        let coin = $(this).children('option:selected').val();
+        switchCurrency(coin);
     });
 });
 function initCrytoData(){
@@ -42,7 +46,7 @@ function initBannerData(){
 
 function initRate(res) {
     for(var i=0;i<res.length;i++){
-        rateMap.set(res[i].coin,rate);
+        rateMap.set(res[i].coin,res[i].rate);
     }
 }
 
@@ -61,12 +65,12 @@ function dealCry(htmlobj){
         var obj=list[i];
         baseBody+=`<tr align="center">
                      <td class="frist">${i+1}</td>
-                     <td data_dollar="${obj.simpleName}">${obj.simpleName}</td>
+                     <td data_name="${obj.simpleName}">${obj.simpleName}</td>
                      <td data_dollar="${obj.marketCap}">${obj.marketCap}</td>
                      <td data_dollar="${obj.price}">${obj.price}</td>
                      <td data_dollar="${obj.volume24}">${obj.volume24}</td>
                      <td data_dollar="${obj.circulatingSupply}">${obj.circulatingSupply}</td>
-                     <td data_dollar="${obj.change24}">${obj.change24}</td>
+                     <td data_rate="${obj.change24}">${obj.change24}</td>
                      <td></td>
                  </tr>`;
     }
@@ -90,18 +94,36 @@ function dealExchange(htmlobj) {
         var obj=list[i];
         baseBody+=`<tr align="center">
                      <td class="frist">${i+1}</td>
-                     <td data_dollar="${obj.name}">${obj.name}</td>
+                     <td data_name="${obj.name}">${obj.name}</td>
                      <td data_dollar=">${obj.adjvol}">${obj.adjvol}</td>
                      <td data_dollar="${obj.volume24h}">${obj.volume24h}</td>
                      <td data_dollar="${obj.volume7d}">${obj.volume7d}</td>
                      <td data_dollar="${obj.volume30d}">${obj.volume30d}</td>
                      <td data_dollar="${obj.noMarkets}">${obj.noMarkets}</td>
-                     <td data_dollar="${obj.change24h}">${obj.change24h}</td>
-                     <td data_dollar="${obj.launched}">${obj.launched}</td>
+                     <td data_rate="${obj.change24h}">${obj.change24h}</td>
+                     <td data_name="${obj.launched}">${obj.launched}</td>
                      <td data_dollar=""></td>
                  </tr>`;
     }
     $("#exchangesData").html(baseBody);
 }
 
+function switchCurrency(coin){
+    let rate = rateMap.get("data-"+ coin);
+    let pre = "$ ";
+    let suffix="";
+    switch(coin){
+        case "cny":
+            pre="￥";suffix=" ";break;
+        case "usd":
+            pre="$";suffix=" ";break;
+        case "btc":
+            pre="";suffix=" BTC";break;
 
+    }
+    $("td[data_dollar]").each(
+        function (index,ele) {
+            ele.innerText=pre + " "+$(ele).attr("data_dollar")*rate + suffix;
+        }
+    );
+}
